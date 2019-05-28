ceff0752861a7bfc07333ab29417fe579bef588d
Sagar Kalra <sagarkalra@Sagars-MacBook-Pro.local>
2019-05-28 09:21:21 +0530

    update

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -61,7 +61,7 @@ export default class MoviesDAO {
       // and _id. Do not put a limit in your own implementation, the limit
       // here is only included to avoid sending 46000 documents down the
       // wire.
-      cursor = await movies.find().limit(1)
+      cursor = await movies.find({countries: {$in: countries}}, {projection: {title:1}})
     } catch (e) {
       console.error(`Unable to issue find command, ${e}`)
       return []
@@ -116,7 +116,7 @@ export default class MoviesDAO {
 
     // TODO Ticket: Text and Subfield Search
     // Construct a query that will search for the chosen genre.
-    const query = {}
+    const query = {genres: {$in: searchGenre}}
     const project = {}
     const sort = DEFAULT_SORT
 
@@ -196,6 +196,9 @@ export default class MoviesDAO {
       sortStage,
       // TODO Ticket: Faceted Search
       // Add the stages to queryPipeline in the correct order.
+      skipStage,
+      limitStage,
+      facetStage,
     ]
 
     try {
@@ -259,7 +262,7 @@ export default class MoviesDAO {
 
     // TODO Ticket: Paging
     // Use the cursor to only return the movies that belong on the current page
-    const displayCursor = cursor.limit(moviesPerPage)
+    const displayCursor = cursor.limit(moviesPerPage).skip(page * moviesPerPage);
 
     try {
       const moviesList = await displayCursor.toArray()
